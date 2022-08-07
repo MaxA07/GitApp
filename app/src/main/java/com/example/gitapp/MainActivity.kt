a777e3f7c8d2693e088c2d6cb71ac55da40559d2
MaxA07 <amorfis77@gmail.com>
2022-08-07 19:39:34 +0300

    activity life cycle

--- app/src/main/java/com/example/gitapp/MainActivity.kt
+++ app/src/main/java/com/example/gitapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "On Create");
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "On Start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "On resume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "On pause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "On stop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "On destroy")
     }
 }
