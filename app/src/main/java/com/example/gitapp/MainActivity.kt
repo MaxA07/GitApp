9dcd4b6652d1a4ebb58560d449768d3ebcf8420c
MaxA07 <amorfis77@gmail.com>
2022-08-08 15:33:51 +0300

    add save state

--- app/src/main/java/com/example/gitapp/MainActivity.kt
+++ app/src/main/java/com/example/gitapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
 import com.example.gitapp.databinding.ActivityMainBinding
@@ -9,7 +10,7 @@ import com.example.gitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    var counter = 0
+    private var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,12 +19,36 @@ class MainActivity : AppCompatActivity() {
             buttonCount.setOnClickListener { count() }
             buttonToast.setOnClickListener { Toast.makeText(this@MainActivity,"$counter", Toast.LENGTH_SHORT).show() }
         }
+
+        if (savedInstanceState == null) {
+            counter = 0
+        }
+        else {
+            counter = savedInstanceState.getInt(KEY_COUNTER)
+        }
+        renderState()
+    }
+
+
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_COUNTER, counter)
     }
 
 
     private fun count() {
         counter++
         binding.number.text = counter.toString()
+    }
+
+    private fun renderState() {
+        binding.number.text = counter.toString()
+    }
+
+    companion object {
+        @JvmStatic private val KEY_COUNTER = "COUNTER"
     }
 
 }
