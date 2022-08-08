553bceb7fe02fa9b4ffbf0bfc2b4db17d9d24138
MaxA07 <amorfis77@gmail.com>
2022-08-08 15:04:44 +0300

    add toast

--- app/src/main/java/com/example/gitapp/MainActivity.kt
+++ app/src/main/java/com/example/gitapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.gitapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.gitapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,11 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.buttonCount.setOnClickListener {
-            count()
+        with(binding) {
+            buttonCount.setOnClickListener { count() }
+            buttonToast.setOnClickListener { Toast.makeText(this@MainActivity,"$counter", Toast.LENGTH_SHORT).show() }
         }
-
     }
+
 
     private fun count() {
         counter++
